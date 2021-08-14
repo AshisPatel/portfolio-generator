0f9c44c8f45a2bfb9760ac4bf0e7bc2bc45a83a8
AP <ashis.n.patel@gmail.com>
2021-08-13 21:03:23 -0500

    Setup basic function to print profile data

---
+++ app.js
@@ -0,0 +1,7 @@
+const printProfileData = profileDataArr => {
+    profileDataArr.forEach(profileItem => console.log(profileItem));
+}
+
+const profileInfo = ['Ashis Patel', 'Web Developer'];
+
+printProfileData(profileInfo); 
