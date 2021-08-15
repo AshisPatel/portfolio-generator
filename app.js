ec5289cb48504e95f9366905a09a738b1b386699
AP <ashis.n.patel@gmail.com>
2021-08-14 19:45:46 -0500

    Add verification to required questions

--- app.js
+++ app.js
@@ -17,12 +17,28 @@ const promptUser = () => {
         {
             type: 'input',
             name: 'name',
-            message: 'What is your name?'
+            message: 'What is your name? (Required)',
+            validate: nameInput => {
+                if(nameInput) {
+                    return true; 
+                } else {
+                    console.log('Please enter your name!'); 
+                    return false; 
+                }
+            }
         }, 
         {
             type: 'input',
             name: 'github',
-            message: 'Enter your Github Username'
+            message: 'Enter your Github Username (Required):',
+            validate: githubInput => {
+                if(githubInput) {
+                    return true;
+                } else {
+                    console.log('Please enter your github username!');
+                    return false; 
+                }
+            }
         },
         {
             type: 'input', 
@@ -55,7 +71,15 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'description',
-            message: 'Provide a description of the project (Required)'
+            message: 'Provide a description of the project (Required):',
+            validate: descriptionInput => {
+                if(descriptionInput) {
+                    return true; 
+                } else {
+                    console.log("Please enter a project description!");
+                    return false; 
+                }
+            }
         },
         {
             type: 'checkbox',
@@ -66,7 +90,15 @@ const promptProject = portfolioData => {
         {
             type: 'input',
             name: 'link',
-            message: 'Enter the Github link to your project. (Required)'
+            message: 'Enter the Github link to your project (Required):',
+            validate: linkInput => {
+                if(linkInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the project github link!");
+                    return false; 
+                }
+            }
         },
         {
             type: 'confirm',
